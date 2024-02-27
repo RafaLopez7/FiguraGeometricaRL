46e8dd734e73caeaa2b4a56a875b3a8e80f946f2
Rafael Lopez Gonzalez <rlopgon274@g.educaand.es>
2024-02-27 14:01:11 +0100

    TEST UNITARIOS AÑADIDOS

--- src/test/java/org/example/FiguraTest.java
+++ src/test/java/org/example/FiguraTest.java
@@ -1,4 +1,22 @@
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FiguraTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculoPermitro() {
+        assertEquals(20, Figura.CalculoPerimetro(5, 5));
+        assertEquals(20, Figura.CalculoPerimetro(4, 6));
+    }
+
+    @org.junit.jupiter.api.Test
+    void tipoFigura() {
+        assertEquals("cuadrado", Figura.tipoFigura(5, 5));
+        assertEquals("rectángulo", Figura.tipoFigura(4, 6));
+    }
 }
